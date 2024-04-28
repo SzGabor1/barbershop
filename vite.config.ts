758c6ab5965a5b625dece352819ddb3c7eb13d3d
SzGabor1 <gabor.szendrei1@gmail.com>
2024-04-28 22:38:33 +0200

    Update import statement in vite.config.ts to use default import syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths'; // Change the import statement to use default import syntax
+import viteTsconfigPaths from 'vite-tsconfig-paths'; // Updated import statement to use default import syntax
 import svgrPlugin from 'vite-plugin-svgr';
 
 export default defineConfig({
   server: {
     port: 4175,
-  }
+  },
   plugins: [
     react({
       jsxRuntime: 'classic'
     }),
-    viteTsconfigPaths(),
+    viteTsconfigPaths.default(),
     svgrPlugin()
   ],
   build: {
     terserOptions: {
-
       compress: {
         unused: true,
       }
